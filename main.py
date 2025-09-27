ea2003b25d7c54765ef21f5688480297018dcb3b
Shruti Chaturvedi <66940685+ShrutiC-git@users.noreply.github.com>
2025-09-27 09:36:02 +0530

    add minio

--- main.py
+++ main.py
@@ -2,12 +2,23 @@ import os, pika, json, csv, joblib
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from urllib.parse import quote_plus
+from minio import Minio
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq.messaging.svc.cluster.local")
 RABBITMQ_USER = os.getenv("RABBITMQ_USER", "user")
 RABBITMQ_PASSWORD = os.getenv("RABBITMQ_PASSWORD", "guest")
-ARTIFACT_PATH = "/artifacts/model.pkl"
+# ARTIFACT_PATH = "/artifacts/model.pkl"
 DATA_PATH = "/artifacts/data.csv"
+
+
+client = Minio(
+    "minio.infra.svc.cluster.local:9000",
+    access_key=os.getenv("MINIO_ACCESS_KEY", "minioadmin"),
+    secret_key=os.getenv("MINIO_SECRET_KEY", "minioadmin123"),
+    secure=False
+)
+
+BUCKET = "fraud-models"
 
 def consume_and_train():
     events = []
@@ -52,8 +63,13 @@ def consume_and_train():
     X = df[["amount"]]
     y = (df["amount"] > 1000).astype(int) # Fake threshold for fraud
     model = LogisticRegression().fit(X,y)
-    joblib.dump(model, ARTIFACT_PATH)
-    print(f"Model trained and saved to {ARTIFACT_PATH}")
-
+    
+    # Ensure bucket exists
+    if not client.bucket_exists(BUCKET):
+        client.make_bucket(BUCKET)
+    joblib.dump(model, "/tmp/model.pkl")
+    client.fput_object(BUCKET, "model.pkl", "/tmp/model.pkl")
+    print("Model trained and uploaded to MinIO.")
+    
 if __name__ == "__main__":
     consume_and_train()
